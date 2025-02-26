aeed21220c002696ee800220c823131438b0425c
John <john@example.com>
2025-02-27 00:15:17 +0530

    endpoint

--- app/components/bud/hooks/useEndPoint.tsx
+++ app/components/bud/hooks/useEndPoint.tsx
@@ -1,34 +1,34 @@
-import { apiKey, tempApiBaseUrl } from "../environment";
+import { tempApiBaseUrl } from "../environment";
 import axios from "axios";
 import { useContext } from "react";
 import ChatContext from "@/app/context/ChatContext";
-
-function successToast(message: string) {
-  console.log(message);
-}
 
 export function useEndPoints() {
   const { chat, endpoints, setEndpoints } = useContext(ChatContext);
 
   async function getEndPoints({ page = 1, limit = 25 }) {
-    const result = await axios
-      .get(`${tempApiBaseUrl}/playground/deployments`, {
-        params: {
-          page: page,
-          limit: limit,
-        },
-        headers: {
-          "api-key": chat?.apiKey,
-        },
-      })
-      .then((res) => {
-        console.log(res.data.endpoints);
-        setEndpoints(res.data.endpoints);
-        return res.data.endpoints;
-      });
+    try {
+      const result = await axios
+        .get(`${tempApiBaseUrl}/playground/deployments`, {
+          params: {
+            page: page,
+            limit: limit,
+          },
+          headers: {
+            "api-key": chat?.apiKey,
+          },
+        })
+        .then((res) => {
+          console.log(res.data.endpoints);
+          setEndpoints(res.data.endpoints);
+          return res.data.endpoints;
+        });
 
-    console.log(result);
-    return result;
+      console.log(result);
+      return result;
+    } catch (error) {
+      return error;
+    }
   }
 
   return { getEndPoints, endpoints };
