d32881f90f5f65fbfa57b62ff0adfc2906570c9b
athul-ab <59333878+athul-ab@users.noreply.github.com>
2025-07-30 16:44:06 +0530

    Update users.nix

--- nix/nixos/common/modules/users.nix
+++ nix/nixos/common/modules/users.nix
@@ -11,3 +11,15 @@
     };
   };
 }
+{
+  users.users = {
+    "athul" = {
+      isNormalUser = true;
+      extraGroups = [ "wheel" ];
+
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJzvzpqulvlwby+7PQUKD6JOPJvyjAi70M+TlsenDIxn athul@accubits.com"
+      ];
+    };
+  };
+}
