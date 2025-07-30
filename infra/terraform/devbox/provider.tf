9c4f7dce5ea96b122b6dec471503646953b00af3
sinanmohd <sinan@sinanmohd.com>
2025-07-30 12:15:50 +0530

    chore(infra/devbox): add root dns record

--- infra/terraform/devbox/provider.tf
+++ infra/terraform/devbox/provider.tf
@@ -21,9 +21,16 @@ terraform {
     sops = {
       source = "carlpett/sops"
     }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+    }
   }
 }
 
 data "sops_file" "secrets" {
   source_file = "${path.module}/secrets.yaml"
+}
+
+provider "cloudflare" {
+  api_token = data.sops_file.secrets.data["cf_token"]
 }
