c0dbe2d21f3a0090fd6a481482c5efb869024fac
sinanmohd <sinan@sinanmohd.com>
2025-08-01 16:23:22 +0530

    chore(infra/dev): init ingress

--- infra/terraform/devbox/dns.tf
+++ infra/terraform/devbox/dns.tf
@@ -21,3 +21,66 @@ resource "cloudflare_dns_record" "argo" {
   content = module.azure.master_ip
   proxied = false
 }
+
+resource "cloudflare_dns_record" "admin" {
+  zone_id = local.zone_id
+  name = "admin.dev"
+  ttl = 3600
+  type = "A"
+  content = module.azure.master_ip
+  proxied = false
+}
+
+resource "cloudflare_dns_record" "playground" {
+  zone_id = local.zone_id
+  name = "playground.dev"
+  ttl = 3600
+  type = "A"
+  content = module.azure.master_ip
+  proxied = false
+}
+
+resource "cloudflare_dns_record" "app" {
+  zone_id = local.zone_id
+  name = "app.dev"
+  ttl = 3600
+  type = "A"
+  content = module.azure.master_ip
+  proxied = false
+}
+
+resource "cloudflare_dns_record" "ask" {
+  zone_id = local.zone_id
+  name = "ask.dev"
+  ttl = 3600
+  type = "A"
+  content = module.azure.master_ip
+  proxied = false
+}
+
+resource "cloudflare_dns_record" "gateway" {
+  zone_id = local.zone_id
+  name = "gateway.dev"
+  ttl = 3600
+  type = "A"
+  content = module.azure.master_ip
+  proxied = false
+}
+
+resource "cloudflare_dns_record" "novu_ws" {
+  zone_id = local.zone_id
+  name = "ws.novu.dev"
+  ttl = 3600
+  type = "A"
+  content = module.azure.master_ip
+  proxied = false
+}
+
+resource "cloudflare_dns_record" "novu_api" {
+  zone_id = local.zone_id
+  name = "api.novu.dev"
+  ttl = 3600
+  type = "A"
+  content = module.azure.master_ip
+  proxied = false
+}
