ce52c31835c8826f87394176e3659cd24a335855
sinanmohd <sinan@sinanmohd.com>
2025-08-01 16:34:42 +0530

    chore(terraform): clean up

--- infra/terraform/devbox/dns.tf
+++ infra/terraform/devbox/dns.tf
@@ -3,82 +3,21 @@ locals {
   zone_id = "1f83390b02ecdecfb95d3964721d9fcb"
 }
 
-resource "cloudflare_dns_record" "master" {
-  zone_id = local.zone_id
-  name = "dev"
-  ttl = 3600
-  type = "A"
-  content = module.azure.master_ip
-  proxied = false
-}
+resource "cloudflare_dns_record" "dev" {
+  for_each = toset([
+    "dev",
+    "argo.dev",
+    "admin.dev",
+    "playground.dev",
+    "gateway.dev",
+    "app.dev",
+    "ask.dev",
+    "api.novu.dev",
+    "ws.novu.dev",
+  ])
 
-
-resource "cloudflare_dns_record" "argo" {
   zone_id = local.zone_id
-  name = "argo.dev"
-  ttl = 3600
-  type = "A"
-  content = module.azure.master_ip
-  proxied = false
-}
-
-resource "cloudflare_dns_record" "admin" {
-  zone_id = local.zone_id
-  name = "admin.dev"
-  ttl = 3600
-  type = "A"
-  content = module.azure.master_ip
-  proxied = false
-}
-
-resource "cloudflare_dns_record" "playground" {
-  zone_id = local.zone_id
-  name = "playground.dev"
-  ttl = 3600
-  type = "A"
-  content = module.azure.master_ip
-  proxied = false
-}
-
-resource "cloudflare_dns_record" "app" {
-  zone_id = local.zone_id
-  name = "app.dev"
-  ttl = 3600
-  type = "A"
-  content = module.azure.master_ip
-  proxied = false
-}
-
-resource "cloudflare_dns_record" "ask" {
-  zone_id = local.zone_id
-  name = "ask.dev"
-  ttl = 3600
-  type = "A"
-  content = module.azure.master_ip
-  proxied = false
-}
-
-resource "cloudflare_dns_record" "gateway" {
-  zone_id = local.zone_id
-  name = "gateway.dev"
-  ttl = 3600
-  type = "A"
-  content = module.azure.master_ip
-  proxied = false
-}
-
-resource "cloudflare_dns_record" "novu_ws" {
-  zone_id = local.zone_id
-  name = "ws.novu.dev"
-  ttl = 3600
-  type = "A"
-  content = module.azure.master_ip
-  proxied = false
-}
-
-resource "cloudflare_dns_record" "novu_api" {
-  zone_id = local.zone_id
-  name = "api.novu.dev"
+  name = each.key
   ttl = 3600
   type = "A"
   content = module.azure.master_ip
