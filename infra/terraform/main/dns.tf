bd226d3aaf7b6ecdb7661713dd51cef264c2fe7d
sinanmohd <sinan@sinanmohd.com>
2025-08-08 17:30:04 +0530

    feat(infra/terraform): init testing and staging env

--- infra/terraform/main/dns.tf
+++ infra/terraform/main/dns.tf
@@ -1,19 +1,38 @@
-resource "cloudflare_dns_record" "dev" {
-  for_each = toset([
-    # prod required
-    "admin.${var.environment}",
-    "customer.${var.environment}",
-    "playground.${var.environment}",
-    "gateway.${var.environment}",
-    "app.${var.environment}",
-    "ask.${var.environment}",
-    "api.novu.${var.environment}",
-    "ws.novu.${var.environment}",
+locals {
+  environments = [
+    "stage", # strictly follows stable git branch, this is our current prod
+    "dev",   # strictly follows master git branch
+    "test",  # loosely follows master git branch, for testing infra changes
+  ]
 
-    # for dev only
-    var.environment,
-    "cloak.${var.environment}",
-  ])
+  # a service can be only removed if it's not required by all the environments
+  # there's no need for separating them now
+  services = [
+    # bud stack requirement
+    "admin",
+    "customer",
+    "playground",
+    "gateway",
+    "app",
+    "ask",
+    "api.novu",
+    "ws.novu",
+
+    # for development only
+    "",
+    "cloak",
+  ]
+
+  services_with_envs = toset(flatten([
+    for env in local.environments : [
+      for srv in local.services :
+      srv == "" ? env : "${srv}.${env}"
+    ]
+  ]))
+}
+
+resource "cloudflare_dns_record" "dev" {
+  for_each = local.services_with_envs
 
   zone_id = var.zone_id
   name    = each.key
