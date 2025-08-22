f5ab48e8a492468f0cd13c390fff8b9bcf33000f
Karthik <karthik@accubits.com>
2025-08-22 21:30:18 +0000

    fix lint issues

--- services/budapp/budapp/migrations/versions/4519dc49ca67_merge_divergent_branches.py
+++ services/budapp/budapp/migrations/versions/4519dc49ca67_merge_divergent_branches.py
@@ -7,8 +7,8 @@ Create Date: 2025-08-22 20:37:19.099349
 """
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
+from alembic import op
 
 
 # revision identifiers, used by Alembic.
