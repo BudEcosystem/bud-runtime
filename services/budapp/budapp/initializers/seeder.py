d51aa97546003ceb7ae848dcbf924ea20b33c00c
lostnull <20404042+lostnull@users.noreply.github.com>
2025-08-22 13:14:43 +0530

    refactor: revert seeder to original state

--- services/budapp/budapp/initializers/seeder.py
+++ services/budapp/budapp/initializers/seeder.py
@@ -2,6 +2,7 @@ from budapp.initializers.base_keycloak_seeder import BaseKeycloakSeeder
 from budapp.initializers.cloud_provider_seeder import CloudProviderSeeder
 from budapp.initializers.dataset_seeder import DatasetSeeder
 
+from budapp.initializers.provider_seeder import ProviderSeeder
 from budapp.initializers.guardrails_seeder import GuardrailsSeeder
 from budapp.initializers.icon_seeder import IconSeeder
 from budapp.initializers.quantization_method_seeder import QuantizationMethodSeeder
@@ -9,14 +10,14 @@ from budapp.initializers.template_seeder import TemplateSeeder
 
 
 seeders = {
-    "keycloak": BaseKeycloakSeeder,
-    "user": UserSeeder,
-    # "provider": ProviderSeeder,   # Commented out for budconnect sync
-    # "cloud_model": CloudModelSeeder,   # Commented out for budconnect sync
-    "icon": IconSeeder,
-    "template": TemplateSeeder,
-    "cloud_provider": CloudProviderSeeder,
-    "quantization_method": QuantizationMethodSeeder,
-    "datasets": DatasetSeeder,
+    # "keycloak": BaseKeycloakSeeder,
+    # "user": UserSeeder,
+    # "provider": ProviderSeeder,  # Commented out for budconnect sync
+    # "cloud_model": CloudModelSeeder,  # Commented out for budconnect sync
+    # "icon": IconSeeder,
+    # "template": TemplateSeeder,
+    # "cloud_provider": CloudProviderSeeder,
+    # "quantization_method": QuantizationMethodSeeder,
+    # "datasets": DatasetSeeder,
     "guardrails": GuardrailsSeeder,
 }
