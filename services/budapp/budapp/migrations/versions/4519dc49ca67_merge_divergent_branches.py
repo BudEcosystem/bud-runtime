216b616871f17ca02d3c02d635a24e614eb444ed
Karthik <karthik@accubits.com>
2025-08-22 21:59:29 +0000

    fix lint issues

--- services/budapp/budapp/migrations/versions/4519dc49ca67_merge_divergent_branches.py
+++ services/budapp/budapp/migrations/versions/4519dc49ca67_merge_divergent_branches.py
@@ -5,6 +5,7 @@ Revises: 65c3cfbe96c9, 6bce128f07e1
 Create Date: 2025-08-22 20:37:19.099349
 
 """
+
 from typing import Sequence, Union
 
 import sqlalchemy as sa
@@ -12,8 +13,8 @@ from alembic import op
 
 
 # revision identifiers, used by Alembic.
-revision: str = '4519dc49ca67'
-down_revision: Union[str, None] = ('d13556621f8a', '65c3cfbe96c9')
+revision: str = "4519dc49ca67"
+down_revision: Union[str, None] = ("d13556621f8a", "65c3cfbe96c9")
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
