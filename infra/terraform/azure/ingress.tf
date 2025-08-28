277d1071022fd772d30d16f8c47d7f2dd2b0fb43
sinanmohd <sinan@sinanmohd.com>
2025-08-28 12:34:13 +0530

    chore(infra/terraform): refactor

--- infra/terraform/azure/ingress.tf
+++ infra/terraform/azure/ingress.tf
@@ -1,108 +1,85 @@
-resource "azurerm_subnet" "ingress" {
-  name                 = "${var.prefix}-ingress"
-  resource_group_name  = azurerm_resource_group.common.name
-  virtual_network_name = azurerm_virtual_network.common.name
-  address_prefixes     = ["10.177.2.0/24", "fd12:babe:cafe::/48"]
+resource "azurerm_public_ip" "ingress_ipv4" {
+  for_each            = local.ingress
+  ip_version          = "IPv4"
+  name                = "${var.prefix}-ingress-${each.key}-ipv4"
+  location            = azurerm_resource_group.common.location
+  resource_group_name = azurerm_resource_group.common.name
+  allocation_method   = "Static"
 }
 
-resource "azurerm_network_security_group" "ingress" {
-  name                = "${var.prefix}-ingress"
+resource "azurerm_public_ip" "ingress_ipv6" {
+  for_each            = local.ingress
+  ip_version          = "IPv6"
+  name                = "${var.prefix}-ingress-${each.key}-ipv6"
+  location            = azurerm_resource_group.common.location
+  resource_group_name = azurerm_resource_group.common.name
+  allocation_method   = "Static"
+}
+
+resource "azurerm_network_interface" "ingress" {
+  for_each            = local.ingress
+  name                = "${var.prefix}-ingress-${each.key}"
   location            = azurerm_resource_group.common.location
   resource_group_name = azurerm_resource_group.common.name
 
-  security_rule {
-    name                       = "${var.prefix}-inbound_ssh"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
+  ip_configuration {
+    primary                       = true
+    name                          = "IPv4"
+    private_ip_address_version    = "IPv4"
+    subnet_id                     = azurerm_subnet.common.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.ingress_ipv4[each.key].id
   }
 
-  security_rule {
-    name                       = "${var.prefix}-inbound_wireguard"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Udp"
-    source_port_range          = "*"
-    destination_port_range     = "51820"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
+  ip_configuration {
+    primary                       = false
+    name                          = "IPv6"
+    private_ip_address_version    = "IPv6"
+    subnet_id                     = azurerm_subnet.common.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.ingress_ipv6[each.key].id
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "ingress" {
+  for_each                  = local.ingress
+  network_interface_id      = azurerm_network_interface.ingress[each.key].id
+  network_security_group_id = azurerm_network_security_group.common.id
+}
+
+resource "azurerm_linux_virtual_machine" "ingress" {
+  for_each                        = local.ingress
+  name                            = "${var.prefix}-ingress-${each.key}"
+  computer_name                   = "${var.prefix}-ingress-${each.key}"
+  location                        = azurerm_resource_group.common.location
+  resource_group_name             = azurerm_resource_group.common.name
+  network_interface_ids           = [azurerm_network_interface.ingress[each.key].id]
+  size                            = each.value
+  secure_boot_enabled             = false
+  vtpm_enabled                    = false
+  disable_password_authentication = true
+  admin_username                  = var.user.name
+
+  boot_diagnostics {
+    storage_account_uri = null
   }
 
-  security_rule {
-    name                       = "${var.prefix}-inbound_mosh"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Udp"
-    source_port_range          = "*"
-    destination_port_range     = "60000-61000"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
+  admin_ssh_key {
+    username   = var.user.name
+    public_key = var.user.ssh_key
   }
 
-  security_rule {
-    name                       = "${var.prefix}-inbound_http"
-    priority                   = 80
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
+  os_disk {
+    name                 = "${var.prefix}-ingress-${each.key}-root"
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS"
+    disk_size_gb         = var.disk_size.ingress
   }
 
-  security_rule {
-    name                       = "${var.prefix}-inbound_https"
-    priority                   = 443
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-  security_rule {
-    name                       = "${var.prefix}-inbound_local_ipv4"
-    priority                   = 150
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "*"
-    source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefix      = local.private_ip_space.v4
-    destination_address_prefix = "*"
-  }
-
-  security_rule {
-    name                       = "${var.prefix}-inbound_local_ipv6"
-    priority                   = 200
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "*"
-    source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefix      = local.private_ip_space.v6
-    destination_address_prefix = "*"
-  }
-
-  security_rule {
-    name                       = "${var.prefix}-outbound"
-    priority                   = 100
-    direction                  = "Outbound"
-    access                     = "Allow"
-    protocol                   = "*"
-    source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "ubuntu-24_04-lts"
+    sku       = "server"
+    version   = "latest"
   }
 }
