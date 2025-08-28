8fd73f3816fe662a1b7b88665cf671419c81eede
sinanmohd <sinan@sinanmohd.com>
2025-08-28 12:34:13 +0530

    chore(infra/terraform): refactor

--- infra/terraform/azure/vars.tf
+++ infra/terraform/azure/vars.tf
@@ -1,8 +1,8 @@
 locals {
   # hostname => sku
-  workers = merge(
+  ingress = merge(
     flatten([
-      for sku, count in var.workers : [for i in range(count) : {
+      for sku, count in var.ingress_sku : [for i in range(count) : {
         "${replace(sku, "/[^a-zA-Z0-9]/", "")}-${i}" = sku
       }]]
     )...
@@ -10,33 +10,45 @@ locals {
 }
 
 variable "prefix" {
-  default = "terraform"
+  type        = string
+  description = "Name to prefix cloud resources"
+  default     = "tofu"
 }
 
-variable "master" {
+variable "disk_size" {
+  description = "Disk size assosiated with vms"
+  # in GB
   default = {
-    hostname = "master"
-    disksize = 128
-    sku      = "Standard_D32als_v6"
+    primary      = 64
+    primary_data = 128
+    ingress      = 32
   }
 }
 
-variable "admin_user" {
-  default = "bud"
-}
-
-variable "ssh_pub_key" {
-  default = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL8LnyOuPmtKRqAZeHueNN4kfYvpRQVwCivSTq+SZvDU sinan@cez"
-}
-
-variable "workers" {
+variable "user" {
   type        = map(string)
-  description = "Map of worker sku to sku count"
+  description = "User information"
 
   default = {
+    name    = "bud"
+    ssh_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL8LnyOuPmtKRqAZeHueNN4kfYvpRQVwCivSTq+SZvDU sinan@cez"
+  }
+}
+
+variable "primary_sku" {
+  type        = string
+  description = "Sku for the primary vm"
+  default     = "Standard_DS1_v2"
+}
+
+variable "ingress_sku" {
+  type        = map(string)
+  description = "Map of ingress sku to sku count"
+
+  default = {
+    Standard_DS1_v2            = 1 // CPU
     Standard_NC24ads_A100_v4   = 0 // NVIDIA A100
     Standard_NV12ads_A10_v5    = 0 // NVIDIA A10
-    Standard_DS1_v2            = 0 // CPU
     Standard_ND96isr_MI300X_v5 = 0 // AMD MI300
   }
 }
