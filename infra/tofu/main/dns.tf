af3afcc2851b1e725daaf3bfd4a94930ad96298b
sinanmohd <sinan@sinanmohd.com>
2025-08-29 12:30:50 +0530

    feat(tofu/main): multiple ingress nodes

--- infra/tofu/main/dns.tf
+++ infra/tofu/main/dns.tf
@@ -1,58 +1,59 @@
-# locals {
-#   environments = [
-#     "stage", # strictly follows stable git branch, this is our current prod
-#     "dev",   # strictly follows master git branch
-#
-#     # personal dev environment
-#     "sinan",
-#     "ditto",
-#     "varun",
-#     "adarsh"
-#   ]
-#
-#   # a service can be only removed if it's not required by all the environments
-#   # there's no need for separating them now
-#   services = [
-#     # bud stack requirement
-#     "admin",
-#     "customer",
-#     "playground",
-#     "gateway",
-#     "app",
-#     "ask",
-#     "api.novu",
-#     "ws.novu",
-#
-#     # for development only
-#     "",
-#     "cloak",
-#   ]
-#
-#   services_with_envs = toset(flatten([
-#     for env in local.environments : [
-#       for srv in local.services :
-#       srv == "" ? "${env}.${var.zone_domain}" : "${srv}.${env}.${var.zone_domain}"
-#     ]
-#   ]))
-# }
-#
-# resource "cloudflare_dns_record" "ipv4" {
-#   for_each = local.services_with_envs
-#   zone_id  = var.zone_id
-#   name     = each.key
-#   ttl      = 3600
-#   type     = "A"
-#   content  = module.azure.master_ip.v4
-#   proxied  = false
-# }
-#
-#
-# resource "cloudflare_dns_record" "ipv6" {
-#   for_each = local.services_with_envs
-#   zone_id  = var.zone_id
-#   name     = each.key
-#   ttl      = 3600
-#   type     = "AAAA"
-#   content  = module.azure.master_ip.v6
-#   proxied  = false
-# }
+locals {
+  environments = [
+    "stage", # strictly follows stable git branch, this is our current prod
+    "dev",   # strictly follows master git branch
+
+    # personal dev environment
+    "sinan",
+    "ditto",
+    "adarsh"
+  ]
+  # a service can be only removed if it's not required by all the environments
+  # there's no need for separating them now
+  services = [
+    "admin",
+    "customer",
+    "playground",
+    "gateway",
+    "app",
+    "ask",
+    "api.novu",
+    "ws.novu",
+  ]
+  services_with_envs = toset(flatten([
+    for env in local.environments : [
+      for srv in local.services :
+      srv == "" ? "${env}.${var.zone_domain}" : "${srv}.${env}.${var.zone_domain}"
+    ]
+  ]))
+
+  ingress_v4 = concat(
+    [for _, ip in module.azure.ip.ingress.v4 : ip],
+    [module.azure.ip.primary.v4]
+  )
+  ingress_v6 = concat(
+    [for _, ip in module.azure.ip.ingress.v6 : ip],
+    [module.azure.ip.primary.v6]
+  )
+}
+
+resource "cloudflare_dns_record" "ipv4" {
+  for_each = setproduct(local.services_with_envs, local.ingress_v4)
+  zone_id  = var.zone_id
+  name     = each.key[0]
+  ttl      = 3600
+  type     = "A"
+  content  = each.key[1]
+  proxied  = false
+}
+
+
+resource "cloudflare_dns_record" "ipv6" {
+  for_each = setproduct(local.services_with_envs, local.ingress_v6)
+  zone_id  = var.zone_id
+  name     = each.key[0]
+  ttl      = 3600
+  type     = "AAAA"
+  content  = each.key[1]
+  proxied  = false
+}
