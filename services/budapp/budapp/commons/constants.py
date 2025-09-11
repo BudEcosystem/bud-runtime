1eaf6d349c778e1ee29a05036372f15da7d5d040
Rahul <rahulvramesh@hotmail.com>
2025-09-11 22:18:19 +0000

    fix(budapp-eval): removed the complition setup and result handling for debuging

--- services/budapp/budapp/commons/constants.py
+++ services/budapp/budapp/commons/constants.py
@@ -589,6 +589,7 @@ class WorkflowTypeEnum(StrEnum):
     ADD_ADAPTER = auto()
     DELETE_ADAPTER = auto()
     EVALUATION_CREATION = auto()
+    EVALUATE_MODEL = auto()
     GUARDRAIL_DEPLOYMENT = auto()
 
 
