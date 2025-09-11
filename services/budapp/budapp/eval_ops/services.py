fa51e81e1c1ed70f6711b698edee3d7043a27e01
Rahul <rahulvramesh@hotmail.com>
2025-09-11 21:31:35 +0000

    fix(budapp-eval): removed the complition setup and result handling for debuging

--- services/budapp/budapp/eval_ops/services.py
+++ services/budapp/budapp/eval_ops/services.py
@@ -2095,7 +2095,9 @@ class EvaluationWorkflowService:
 
                 logger.debug(f"Trigger Workflow Response 01 : {trigger_workflow_response}")
 
-                evaluation_events_payload = trigger_workflow_response.copy()
+                evaluation_events_payload = {
+                    BudServeWorkflowStepEventName.EVALUATION_EVENTS.value: trigger_workflow_response
+                }
 
                 db_workflow_step = await WorkflowStepService(self.session).create_or_update_next_workflow_step(
                     workflow.id, current_step_number, evaluation_events_payload
@@ -2105,7 +2107,7 @@ class EvaluationWorkflowService:
 
                 evaluation_events_payload["progress_type"] = BudServeWorkflowStepEventName.EVALUATION_EVENTS.value
                 await WorkflowDataManager(self.session).update_by_fields(
-                    workflow, {"progress": evaluation_events_payload, "current_step": workflow_current_step}
+                    workflow, {"progress": trigger_workflow_response, "current_step": workflow_current_step}
                 )
 
                 logger.debug(f"Trigger Workflow Response 02: {evaluation_events_payload}")
